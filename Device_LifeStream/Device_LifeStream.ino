75cc967512bd94d4b126ae9a2f992268097a1945
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-16 22:26:34 -0300

    Add Buzzer

--- Device_LifeStream/Device_LifeStream.ino
+++ Device_LifeStream/Device_LifeStream.ino
@@ -1,5 +1,3 @@
-
-
 //--------Criar a conexão com o Wi-Fi-------------
 // 
 // 
@@ -11,13 +9,24 @@ int lastButtonState = HIGH; // Estado anterior do botão
 unsigned long lastDebounceTime = 0; // Último momento em que o botão foi lido sem rebote
 unsigned long debounceDelay = 50; // Atraso de debounce em milissegundos
 
+//------------Buzzer-------------------------
+
+const int buzzerPin = 14; 
+
 
 void setup() {
   pinMode(buttonPin, INPUT); // Configura o pino do botão como entrada
   Serial.begin(9600); // Inicia a comunicação serial para depuração (opcional)
+  pinMode(buzzerPin, OUTPUT); // Configura o pino do buzzer como saída
   
 }
 
+void buzzer() {
+
+  digitalWrite(buzzerPin, HIGH); // liga o buzzer
+  delay(1000); // Mantém o buzzer acionado por 1 segundo 
+  digitalWrite(buzzerPin, LOW); // Desliga o buzzer
+}
 
 void loop() {
   
@@ -37,6 +46,7 @@ void loop() {
       // Se o novo estado do botão for HIGH, significa que o botão foi pressionado
       if (buttonState == HIGH) {
         Serial.println("Botão pressionado!");
+        buzzer();
         // Adicione o código que deseja executar quando o botão é pressionado
       }
     }
@@ -45,7 +55,3 @@ void loop() {
   // Atualiza o último estado do botão
   lastButtonState = reading;
 }
-
-
-
-        
