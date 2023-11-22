7a85600ce350e83630d2776bf4db05704ad4800a
Guilherme Santos <107642647+GuilhermeSSantos2004@users.noreply.github.com>
2023-11-22 06:13:33 -0300

    Update Device_LifeStream.ino
    Add protocolo MQTT

--- Device_LifeStream/Device_LifeStream.ino
+++ Device_LifeStream/Device_LifeStream.ino
@@ -1,25 +1,61 @@
-//--------Criar a conexão com o Wi-Fi-------------
-// 
-// 
-//-----------------------------------------------
+#include "ArduinoJson.h"
+#include "EspMQTTClient.h"
+#include <PubSubClient.h>
 
 const int buttonPin = 19; // Defina o pino do botão
 int buttonState = HIGH; // Estado inicial do botão
 int lastButtonState = HIGH; // Estado anterior do botão
 unsigned long lastDebounceTime = 0; // Último momento em que o botão foi lido sem rebote
-unsigned long debounceDelay = 50; // Atraso de debounce em milissegundos
+unsigned long debounceDelay = 30; // Atraso de debounce em milissegundos
 
 //------------Buzzer-------------------------
 
 const int buzzerPin = 14; 
 
+EspMQTTClient client{
+  "Wokwi-GUEST", // SSID do WiFi
+  "",            // Senha do WiFi
+  "mqtt.tago.io", // Endereço do servidor
+  "Default",      // Usuário
+  "d5f6b24a-6bf3-4cdd-ab60-37ef5069a21e", // Token do dispositivo
+  "LifeStream",   // Nome do dispositivo
+  1883            // Porta de comunicação
+};
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(buttonPin, INPUT); // Configura o pino do botão como entrada
-  Serial.begin(9600); // Inicia a comunicação serial para depuração (opcional)
   pinMode(buzzerPin, OUTPUT); // Configura o pino do buzzer como saída
+
+  Serial.println("Conectando WiFi");
+  while (!client.isWifiConnected()) {
+    Serial.print('.');
+    client.loop();
+    delay(1000);
+  }
+  Serial.println("WiFi Conectado");
   
+  Serial.println("Conectando com Servidor MQTT");
+  while (!client.isMqttConnected()) {
+    Serial.print('.');
+    client.loop();
+    delay(1000);
+  }
+  Serial.println("MQTT Conectado");
+
+  // Inscrever-se ao tópico desejado
+  mqttClient.setServer("mqtt.tago.io", 1883);
+  mqttClient.setCallback(callback);
+  mqttClient.subscribe("topicoTDSPI");
 }
+
+void onConnectionEstablished()
+{}
+
 
 void buzzer() {
 
@@ -28,8 +64,23 @@ void buzzer() {
   digitalWrite(buzzerPin, LOW); // Desliga o buzzer
 }
 
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensagem recebida no tópico: ");
+  Serial.println(topic);
+
+  Serial.print("Payload: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+char bufferJson[100];
+int temperatura = 0;
+
 void loop() {
-  
+
   int reading = digitalRead(buttonPin); // Lê o estado do botão
 
   // Verifica se houve mudança no estado do botão
@@ -54,4 +105,6 @@ void loop() {
 
   // Atualiza o último estado do botão
   lastButtonState = reading;
+ 
+
 }
