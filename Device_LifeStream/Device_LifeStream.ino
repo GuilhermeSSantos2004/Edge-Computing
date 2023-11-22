910af92ab38b9f4d1e763cab96f5e41f36e43d93
Guilherme Santos <107642647+GuilhermeSSantos2004@users.noreply.github.com>
2023-11-22 06:59:35 -0300

    Update Device_LifeStream.ino
    Add Subscribing

--- Device_LifeStream/Device_LifeStream.ino
+++ Device_LifeStream/Device_LifeStream.ino
@@ -1,16 +1,5 @@
 #include "ArduinoJson.h"
 #include "EspMQTTClient.h"
-#include <PubSubClient.h>
-
-const int buttonPin = 19; // Defina o pino do botão
-int buttonState = HIGH; // Estado inicial do botão
-int lastButtonState = HIGH; // Estado anterior do botão
-unsigned long lastDebounceTime = 0; // Último momento em que o botão foi lido sem rebote
-unsigned long debounceDelay = 30; // Atraso de debounce em milissegundos
-
-//------------Buzzer-------------------------
-
-const int buzzerPin = 14; 
 
 EspMQTTClient client{
   "Wokwi-GUEST", // SSID do WiFi
@@ -22,15 +11,17 @@ EspMQTTClient client{
   1883            // Porta de comunicação
 };
 
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
+const int buzzerPin = 14;
+
+const int buttonPin = 19; // Defina o pino do botão
+int buttonState = HIGH; // Estado inicial do botão
+int lastButtonState = HIGH; // Estado anterior do botão
+unsigned long lastDebounceTime = 0; // Último momento em que o botão foi lido sem rebote
+unsigned long debounceDelay = 30; // Atraso de debounce em milissegundos
+
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(buttonPin, INPUT); // Configura o pino do botão como entrada
-  pinMode(buzzerPin, OUTPUT); // Configura o pino do buzzer como saída
-
   Serial.println("Conectando WiFi");
   while (!client.isWifiConnected()) {
     Serial.print('.');
@@ -46,15 +37,19 @@ void setup() {
     delay(1000);
   }
   Serial.println("MQTT Conectado");
-
-  // Inscrever-se ao tópico desejado
-  mqttClient.setServer("mqtt.tago.io", 1883);
-  mqttClient.setCallback(callback);
-  mqttClient.subscribe("topicoTDSPI");
 }
 
-void onConnectionEstablished()
-{}
+void onConnectionEstablished(){
+   Serial.println("Subscribing mytopic");
+   client.subscribe("mytopic", onTestMessageReceived);
+}
+
+void onTestMessageReceived(const String& message) {
+  Serial.println("message received from mytopic: " + message);
+}
+
+char bufferJson[100];
+int temperatura = 0;
 
 
 void buzzer() {
@@ -62,49 +57,48 @@ void buzzer() {
   digitalWrite(buzzerPin, HIGH); // liga o buzzer
   delay(1000); // Mantém o buzzer acionado por 1 segundo 
   digitalWrite(buzzerPin, LOW); // Desliga o buzzer
+  delay(1000); // Mantém o buzzer acionado por 1 segundo
+  digitalWrite(buzzerPin, HIGH); 
+  delay(1000);  
+  digitalWrite(buzzerPin, LOW); 
+
+  client.loop();
 }
 
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensagem recebida no tópico: ");
-  Serial.println(topic);
-
-  Serial.print("Payload: ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-
-char bufferJson[100];
-int temperatura = 0;
 
 void loop() {
 
+
   int reading = digitalRead(buttonPin); // Lê o estado do botão
 
-  // Verifica se houve mudança no estado do botão
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis(); // Atualiza o momento do último estado sem rebote
-  }
-
-  // Verifica se o botão está em um novo estado após o período de debounce
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // Se o estado do botão mudou, atualiza o estado atual
-    if (reading != buttonState) {
+  
+  if (reading != buttonState) {
       buttonState = reading;
 
       // Se o novo estado do botão for HIGH, significa que o botão foi pressionado
       if (buttonState == HIGH) {
         Serial.println("Botão pressionado!");
+
+          lastButtonState = reading;
+
+        temperatura = random(0, 100);
+
+        StaticJsonDocument<300> documentoJson;
+        documentoJson["variable"] = "Button";
+        documentoJson["value"] = lastButtonState;
+        documentoJson["unit"] = "status";
+        serializeJson(documentoJson, bufferJson);
+        Serial.println(bufferJson);
+        client.publish("topicoEngSoft", bufferJson);
+        
         buzzer();
+        
+        
         // Adicione o código que deseja executar quando o botão é pressionado
       }
     }
-  }
+  
 
   // Atualiza o último estado do botão
-  lastButtonState = reading;
- 
 
 }
