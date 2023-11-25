ab35823eba61aeaace1bfebd1796fb31ebcb0745
Guilherme Santos <107642647+GuilhermeSSantos2004@users.noreply.github.com>
2023-11-24 21:58:25 -0300

    Update Device_LifeStream.ino
    Add temp

--- Device_LifeStream/Device_LifeStream.ino
+++ Device_LifeStream/Device_LifeStream.ino
@@ -1,5 +1,5 @@
-#include "ArduinoJson.h"
-#include "EspMQTTClient.h"
+#include <ArduinoJson.h>
+#include <EspMQTTClient.h>
 
 EspMQTTClient client{
   "Wokwi-GUEST", // SSID do WiFi
@@ -12,21 +12,27 @@ EspMQTTClient client{
 };
 
 const int buzzerPin = 14;
+const int buttonPin = 19;
 
-const int buttonPin = 19; // Defina o pino do botão
-int buttonState = HIGH; // Estado inicial do botão
-int lastButtonState = HIGH; // Estado anterior do botão
-unsigned long lastDebounceTime = 0; // Último momento em que o botão foi lido sem rebote
-unsigned long debounceDelay = 30; // Atraso de debounce em milissegundos
+int buttonState = HIGH;
+int lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 30;
 
+int horasDigitadas = 0;
+int minutosDigitados = 0;
+int segundosDigitados = 0;
+bool temporizadorAtivo = false;
+unsigned long tempoInicio = 0;
+
+char bufferJson[100];
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Conectando WiFi");
 
-
-  pinMode(buzzerPin, OUTPUT); // Set buzzer pin as output
-  digitalWrite(buzzerPin, LOW); // Ensure the buzzer is initially off
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, LOW);
 
   while (!client.isWifiConnected()) {
     Serial.print('.');
@@ -34,7 +40,7 @@ void setup() {
     delay(1000);
   }
   Serial.println("WiFi Conectado");
-  
+
   Serial.println("Conectando com Servidor MQTT");
   while (!client.isMqttConnected()) {
     Serial.print('.');
@@ -42,68 +48,85 @@ void setup() {
     delay(1000);
   }
   Serial.println("MQTT Conectado");
+
+  client.subscribe("mytopic", onTestMessageReceived);
 }
 
-void onConnectionEstablished(){
-   Serial.println("Subscribing mytopic");
-   client.subscribe("mytopic", onTestMessageReceived);
+void onConnectionEstablished() {
+  Serial.println("Subscribing mytopic");
 }
 
 void onTestMessageReceived(const String& message) {
   Serial.println("message received from mytopic: " + message);
+
+  DynamicJsonDocument doc(300);
+  deserializeJson(doc, message);
+
+  horasDigitadas = doc["horas"].as<int>();
+  minutosDigitados = doc["minutos"].as<int>();
+  segundosDigitados = doc["segundos"].as<int>();
+
+  if (horasDigitadas >= 0 && horasDigitadas < 24 && minutosDigitados >= 0 && minutosDigitados < 60 && segundosDigitados >= 0 && segundosDigitados < 60) {
+    temporizadorAtivo = true;
+    tempoInicio = millis();
+    Serial.println("Temporizador iniciado!");
+    Serial.print("Tempo configurado: ");
+    Serial.print(horasDigitadas);
+    Serial.print(" horas, ");
+    Serial.print(minutosDigitados);
+    Serial.print(" minutos e ");
+    Serial.print(segundosDigitados);
+    Serial.println(" segundos.");
+  } else {
+    Serial.println("Por favor, insira um tempo válido.");
+  }
 }
 
-char bufferJson[100];
-int temperatura = 0;
-
-
 void buzzer() {
-
-  digitalWrite(buzzerPin, HIGH); // liga o buzzer
-  delay(1000); // Mantém o buzzer acionado por 1 segundo 
-  digitalWrite(buzzerPin, LOW); // Desliga o buzzer
-  delay(1000); // Mantém o buzzer acionado por 1 segundo
-  digitalWrite(buzzerPin, HIGH); 
-  delay(1000);  
-  digitalWrite(buzzerPin, LOW); 
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000);
+  digitalWrite(buzzerPin, LOW);
+  delay(1000);
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000);
+  digitalWrite(buzzerPin, LOW);
 
   client.loop();
 }
 
-
 void loop() {
+  int reading = digitalRead(buttonPin);
 
-
-  int reading = digitalRead(buttonPin); // Lê o estado do botão
-
-  
   if (reading != buttonState) {
-      buttonState = reading;
+    buttonState = reading;
 
-      // Se o novo estado do botão for HIGH, significa que o botão foi pressionado
-      if (buttonState == HIGH) {
+    if (buttonState == HIGH) {
+      buzzer();
+      Serial.println("Botão pressionado!");
 
-        buzzer();
-        Serial.println("Botão pressionado!");
+      lastButtonState = reading;
 
-        lastButtonState = reading;
+      DynamicJsonDocument documentoJson(300);
+      documentoJson["variable"] = "Button";
+      documentoJson["value"] = lastButtonState;
+      documentoJson["unit"] = "status";
 
-        StaticJsonDocument<300> documentoJson;
-        documentoJson["variable"] = "Button";
-        documentoJson["value"] = lastButtonState;
-        documentoJson["unit"] = "status";
-        serializeJson(documentoJson, bufferJson);
-        Serial.println(bufferJson);
-        client.publish("topicoEngSoft", bufferJson);
-        
-        
-        
-        
-        // Adicione o código que deseja executar quando o botão é pressionado
-      }
+      serializeJson(documentoJson, bufferJson);
+
+      client.publish("topicoEngSoft", bufferJson);
     }
-  
+  }
 
-  // Atualiza o último estado do botão
+  if (temporizadorAtivo) {
+    unsigned long tempoDecorrido = millis() - tempoInicio;
+    unsigned long tempoDigitadoEmMillis = ((horasDigitadas * 3600) + (minutosDigitados * 60) + segundosDigitados) * 1000;
 
+    if (tempoDecorrido >= tempoDigitadoEmMillis) {
+      Serial.println("Temporizador concluído!");
+      temporizadorAtivo = false;
+
+      // Activate the buzzer when the timer reaches zero
+      buzzer();
+    }
+  }
 }
